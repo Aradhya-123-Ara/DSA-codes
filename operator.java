9acf8dacea1ff4fee08c16cbafa6c24dc6be9cca
Aradhya jain <aradhyajain1833@gmail.com>
2026-05-17 10:10:15 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,162 @@
+//Arithmetic binary operator
+import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 10;
+        
+        System.out.print(a % b);
+    }
+}
+//unary operator
+import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 0;
+        
+        b = a++;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+    }
+}
+//++a
+import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 0;
+        
+        b = ++a;
+        
+        System.out.println(a);
+        System.out.println(b);
+      //--a
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 0;
+    
+        b = --a;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+    }
+}
+//relational operator == , != , >= , <= , < , >
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 0;
+        System.out.println(a == b);
+        
+        
+    }
+}
+//logical operator , tocheck logic , && , || , !
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 4;
+        int c = 6;
+        System.out.println(a > b && b < c);
+        
+        
+    }
+}
+// || dono me ek bhi true ho to
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 4;
+        int c = 6;
+        System.out.println(a > b || b > c);
+        
+        
+    }
+}
+//! reverse the output
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 4;
+        int c = 6;
+        System.out.println(!(a > b));
+        
+        
+    }
+}
+//Bitwise Operators = & , | , ^ , ~ , << , >> to get output in bits
+//& = dono 1 ho to 1 otherwise 0 
+//| = dono me se kisi me bhi 1 ho to 1 otherwise 0
+//^ = jab dono different ho to 1 otherwise 0
+// ~ = bit compliment ke liye , like bit ko convert karna , + to - , or - to + , otherwise 1 to 0, 0 to 1// << to left shift the bit 1 by 1  like 1010 << 1 = for example 1010 after left shift it will be , 0100 , bcz it has some empty spaces by side which value is 0
+// >> = same for rigth shift but by right side
+
+//Assignment operator : =, +=, -=, *=, %= , /=
+  import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 10;
+        
+        System.out.println(a+=b);
+        System.out.println(b+=a); 
+        
+    }
+}
+// +=
+      import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 5;
+        int b = 10;
+        
+        System.out.println(a+=b);
+        
+        
+        
+        
+    }
+}
+
