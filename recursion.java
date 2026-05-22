e1c080c9a1172438696333c6d2a4377fee2436ff
Aradhya jain <aradhyajain1833@gmail.com>
2026-05-22 12:02:36 +0530

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -32,3 +32,56 @@ public class Main{
         printNumber(n);
     }
 }
+//sum of n natural numbers
+import java.util.Scanner;
+
+public class Main{
+    public static void printSum(int n, int sum){
+        if(n == 6){
+            System.out.println(sum);
+            return;
+        }
+        sum += n;
+        printSum(n + 1, sum);
+    }
+    public static void main(String[] args){
+        System.out.print("the sum is :");
+        printSum(1, 0);
+    }
+}
+//to get factorial
+import java.util.Scanner;
+
+public class Main{
+    public static void calcFact(int n, int fact){
+        if(n == 0){
+            System.out.println(fact);
+            return;
+        }
+        fact *= n;
+        calcFact(n - 1, fact);
+    }
+    public static void main(String[] args){
+        calcFact(5, 1);
+    }
+}
+// fibonacci
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+
+public class Main{
+    public static void printFib(int a, int b, int n){
+        if(n == 0){
+        return;
+    }
+    System.out.println(a);
+    printFib(b, a + b, n - 1);
+        
+    }
+    public static void main(String[] args){
+        printFib(0 , 1 , 5);
+    }
+
+}
